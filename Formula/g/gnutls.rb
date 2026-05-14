94cf492892fe998614a0d643b60c5a32b79982c6
Michael Cho <michael@michaelcho.dev>
2026-05-14 11:58:20 -0400

    gnutls: remove DANE support
    DANE support requires `unbound` which adds indirect dependency on
    OpenSSL. This isn't ideal for alternative SSL/TLS libraries.
    
    The feature is also not included in:
    * Alpine
    * Arch Linux
    * Gentoo (default build)
    * MacPorts (default variant)
    
    Other distros usually keep DANE split package separate from main lib:
    * Debian - libgnutls-dane0t64
    * Fedora - gnutls-dane
    
    If there is strong enough demand, a split formula for `gnutls-dane` can
    be introduced for DANE support.

--- Formula/g/gnutls.rb
+++ Formula/g/gnutls.rb
@@ -42,22 +42,17 @@ class Gnutls < Formula
 
   depends_on "pkgconf" => :build
   depends_on "texinfo" => :build
-  depends_on "ca-certificates"
+  depends_on "ca-certificates" => :no_linkage
   depends_on "gmp"
   depends_on "libidn2"
   depends_on "libtasn1"
   depends_on "libunistring"
   depends_on "nettle"
   depends_on "p11-kit"
-  depends_on "unbound"
 
   on_macos do
     depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1400
     depends_on "gettext"
-  end
-
-  on_linux do
-    depends_on "zlib-ng-compat"
   end
 
   fails_with :clang do
@@ -66,7 +61,10 @@ class Gnutls < Formula
   end
 
   def install
+    # DANE support is disabled so GnuTLS does not have an indirect dependency on OpenSSL.
+    # If the feature is wanted, then can consider shipping as split `gnutls-dane` formula.
     args = %W[
+      --disable-libdane
       --disable-silent-rules
       --disable-static
       --sysconfdir=#{etc}
@@ -78,7 +76,7 @@ class Gnutls < Formula
     system "./configure", *args, *std_configure_args
     system "make", "install"
 
-    inreplace [lib/"pkgconfig/gnutls.pc", lib/"pkgconfig/gnutls-dane.pc"], prefix, opt_prefix
+    inreplace lib/"pkgconfig/gnutls.pc", prefix, opt_prefix
 
     # certtool shadows the macOS certtool utility
     mv bin/"certtool", bin/"gnutls-certtool"
