0d944039820204cec68f151e1e8e3d781dbb64a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-15 17:39:36 +0000

    helix-db 3.0.0

--- Formula/h/helix-db.rb
+++ Formula/h/helix-db.rb
@@ -1,8 +1,8 @@
 class HelixDb < Formula
   desc "Open-source graph-vector database built from scratch in Rust"
   homepage "https://helix-db.com"
-  url "https://github.com/HelixDB/helix-db/archive/refs/tags/v2.3.5.tar.gz"
-  sha256 "79d5a19efacdc1ee20f74eacdf26e738e1e8a5191de4e86fad0444857ce3d2d0"
+  url "https://github.com/HelixDB/helix-db/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "bf738c4cdf2c0511f65e848b0802a8fc93a4906e411ead35478c554b1984e812"
   license "AGPL-3.0-only"
 
   bottle do
